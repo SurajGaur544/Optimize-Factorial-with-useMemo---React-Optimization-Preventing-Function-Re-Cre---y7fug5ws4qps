8941871528b89bede214156d13945d3d70669fa4
Suraj Gaur <surajgaur242@gmail.com>
2023-12-09 12:13:20 +0530

    add some code to woek the program

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,15 @@
 import { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
 export default function Home() {
-  
+  const [num, setNum] = useState(1);
      
   return (
     <div>
         <h1>Welcome to Next.js!</h1>
         <section>
                 <h2>Factorial Calculation</h2>
-                <input 
-                    type="number" 
-                    
-                   } 
-                />
-       
-                <FactorialComponent  />
+                <input  type="number" value={num} onChange={(e) => {setNum(e.target.value)}} />
+                <FactorialComponent num={num} />
             </section>
     </div>
 );
